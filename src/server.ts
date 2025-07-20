71a3a790b56d05ea91ae890696c740d0fc780811
Felipe Pinheiro <felipereginadev@gmail.com>
2025-07-20 19:21:08 -0300

    create new routes

--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,15 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { env } from './env.ts'
-import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { getRooms } from './http/routes/get-rooms.ts'
+import { createRoom } from './http/routes/create-room.ts'
+import { getRoomQuestions } from './http/routes/get-room-questions.ts'
+import { createQuestion } from './http/routes/create-question.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
-  origin: 'http://localhost:5173',
+  origin: '*',
 })
 
 app.setSerializerCompiler(serializerCompiler)
@@ -21,6 +24,9 @@ app.get('/health', () => {
   return 'OK'
 })
 
-app.register(getRoomsRoute)
+app.register(getRooms)
+app.register(createRoom)
+app.register(getRoomQuestions)
+app.register(createQuestion)
 
 app.listen({ port: env.PORT })
